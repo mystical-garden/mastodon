2d450ae28f36d45dde937db53d0a7e505437b09d
Lain Iwakura <lain@soykaf.club>
2017-11-14 16:22:34 +0100

    Make character limit configurable (#5697)
    Make character limit configurable.
    
    Return character limit in API and initial state.
    
    Handle character limit in initial state in frontend.
    
    Code style fixes.
    
    Rename MAX_CHARS to MAX_TOOT_CHARS to be more specific.
    
    Fix StatusLengthValidator tests
    
    This fixes the StatusLengthValidator tests to use
    StatusLengthValidator::MAX_CHARS to derive the length of the strings
    tested, and so makes them resilient to changes in MAX_CHARS (such as the
    one that made these tests fail, with it being changed from 500 to 512).
    
    This seems a bit odd for some types of tests, but appears to make sense
    here, where the testing goes beyond `status.length > MAX_CHARS`.
    
    Fix CharacterCount in vanilla UI (#1883)

--- app/serializers/initial_state_serializer.rb
+++ app/serializers/initial_state_serializer.rb
@@ -6,7 +6,7 @@ class InitialStateSerializer < ActiveModel::Serializer
   attributes :meta, :compose, :accounts,
              :media_attachments, :settings,
              :max_feed_hashtags, :poll_limits,
-             :languages, :features
+             :languages, :features, :max_toot_chars
 
   attribute :critical_updates_pending, if: -> { object&.role&.can?(:view_devops) && SoftwareUpdate.check_enabled? }
 
@@ -24,6 +24,10 @@ class InitialStateSerializer < ActiveModel::Serializer
       min_expiration: PollExpirationValidator::MIN_EXPIRATION,
       max_expiration: PollExpirationValidator::MAX_EXPIRATION,
     }
+  end
+
+  def max_toot_chars
+    StatusLengthValidator::MAX_CHARS
   end
 
   def meta
